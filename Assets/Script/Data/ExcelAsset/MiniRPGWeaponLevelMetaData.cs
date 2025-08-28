91c21ad85c2238c3a4c61b19feba580c6778f3dc
joonhyukYun <hanel99@neptunez.co.kr>
2025-08-28 20:40:50 +0900

    장비강화 테이블 추가

---
+++ Assets/Script/Data/ExcelAsset/MiniRPGWeaponLevelMetaData.cs
@@ -0,0 +1,13 @@
+using System;
+
+[Serializable]
+public class MiniRPGWeaponLevelMetaData
+{
+    public int level;
+    public float up;
+    public float stay;
+    public float down;
+    public float atk;
+    public float criDmg;
+    public int requireCoin;
+}
